684897424d1b65543921993d73ae12d4c047cf48
student <student@dep24>
2017-03-04 15:01:50 +0300

    Initial commit

---
+++ head1.h
@@ -0,0 +1,15 @@
+//
+// Created by student on 04.03.17.
+//
+
+#ifndef UNTITLED3_HEAD1_H
+#define UNTITLED3_HEAD1_H
+float fun1(float a, float b) {
+    static int i=0;
+    i++;
+    float x=a-b;
+    return x;
+
+}
+
+#endif //UNTITLED3_HEAD1_H
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "head1.h"
+
+int main() {
+    float a = 115.45;
+    float b = 105.28;
+    float x = fun1(a, b);
+    printf("x=%2.1f\n", x);
+    return 0;
+}
